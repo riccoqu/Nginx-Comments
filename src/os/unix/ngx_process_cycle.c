92bd78db56ade20891682e416e61d31739df5037
riccoqu <ricco.qu@outlook.com>
2016-08-21 17:15:10 +0800

    Nginx进程循环相关

--- src/os/unix/ngx_process_cycle.c
+++ src/os/unix/ngx_process_cycle.c
@@ -362,7 +362,7 @@ ngx_start_worker_processes(ngx_cycle_t *cycle, ngx_int_t n, ngx_int_t type)
     ch.command = NGX_CMD_OPEN_CHANNEL;
 
     for (i = 0; i < n; i++) {
-
+        //产生 worker进程,传递的数据为索引
         ngx_spawn_process(cycle, ngx_worker_process_cycle,
                           (void *) (intptr_t) i, "worker process", type);
         /**
